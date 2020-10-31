28850716b552296362a1692b59602611a9a2d0cc
rojda <rojda@DESKTOP-E5FHO01>
2020-11-01 00:43:11 +0300

    First Commit

---
+++ 101-Java-Bootcamp/src/GithubDeneme/Musteri.java
@@ -0,0 +1,34 @@
+package GithubDeneme;
+
+public class Musteri {
+
+	private String isim;
+	private String soyisim;
+	private String telefonno;
+	private String email;
+	
+	public Musteri(String _isim, String _soyisim, String _telefonno, String _email) 
+	
+	{
+	
+		
+		this.isim=_isim;
+		this.soyisim=_soyisim;
+		this.telefonno=_telefonno;
+		this.email=_email;
+		
+	}
+	
+	public void musteriTanýt()
+	    {
+		
+		System.out.println("Ýsim: "+isim+ " Soyisim: "+soyisim+" Telefon numarasý: "+telefonno + 
+				" Email: "+ email);
+		}
+	
+	public void MerhabaDe() 
+	{
+		System.out.println("Merhaba");
+	}
+	
+}
