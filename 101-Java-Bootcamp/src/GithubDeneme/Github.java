c6554b9e7b756db1d9695d37f25f2913492cf64c
rojda <rojda@DESKTOP-E5FHO01>
2020-11-01 00:56:57 +0300

    First Commit

--- 101-Java-Bootcamp/src/GithubDeneme/Github.java
+++ 101-Java-Bootcamp/src/GithubDeneme/Github.java
@@ -7,12 +7,12 @@ public class Github {
 		Musteri musteri1=new Musteri("Deniz","Yildirim", "05347894569" , "denizyilmaz@gmail.com");
 		
 		musteri1.MerhabaDe();
-		musteri1.musteriTanıt();
+		musteri1.musteriTanit();
 		
 		Musteri musteri2=new Musteri("Aysun","Yazici","05553269887","aysunyazici@gmail.com");
 		
 		musteri2.MerhabaDe();
-		musteri2.musteriTanıt();
+		musteri2.musteriTanit();
 		
 	}
 
